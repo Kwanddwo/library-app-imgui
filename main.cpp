007a20a5e2247500241bae545634de45c79437e6
yassir53 <116460304+yassir53@users.noreply.github.com>
2025-01-02 07:58:18 +0000

    pull

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-
+#include "vector"
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <string>
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -19,6 +20,183 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "{\n"
 "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
 "}\n\0";
+
+enum class Pages {
+	MainTable,
+	CurrentBorrows,
+	BorrowHistory
+};
+
+
+bool checkedP = false;
+bool checkedT = false;
+bool checkedA = false;
+bool checkedC = false;
+bool checkedG = false;
+
+void DrawMainTable(ImGuiTableFlags flags) {
+	const int nb_col = 9;
+
+	if (ImGui::Checkbox("Publish date", &checkedP)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("title", &checkedT)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Author", &checkedA)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Grenre", &checkedG)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Category", &checkedC)) {
+	}
+	char* wanted = (char*)malloc(sizeof(char));
+	ImGui::InputText("##", wanted, 100);
+	ImGui::SameLine();
+	if (ImGui::Button("apply")) {
+
+	}
+	if (ImGui::BeginTable("tableLogBook", nb_col, flags)) {
+		ImGui::TableSetupColumn("Title");
+		ImGui::TableSetupColumn("Publish Date");
+		ImGui::TableSetupColumn("Number of Copies Available");
+		ImGui::TableSetupColumn("Number of Pages");
+		ImGui::TableSetupColumn("Language");
+		ImGui::TableSetupColumn("Editor");
+		ImGui::TableSetupColumn("Authors");
+		ImGui::TableSetupColumn("Genres");
+		ImGui::TableSetupColumn("Categories");
+		ImGui::TableHeadersRow();
+		ImGui::EndTable();
+	}
+}
+
+void DrawBorrowsTable(ImGuiTableFlags flags) {
+	const int nb_col = 9;
+
+	if (ImGui::Checkbox("Publish date", &checkedP)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("title", &checkedT)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Author", &checkedA)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Grenre", &checkedG)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Category", &checkedC)) {
+	}
+	char* wanted = (char*)malloc(sizeof(char));
+	ImGui::InputText("##", wanted, 100);
+	ImGui::SameLine();
+	if (ImGui::Button("apply")) {
+
+	}
+	if (ImGui::BeginTable("tableBorrows", nb_col, flags)) {
+		ImGui::TableSetupColumn("Title");
+		ImGui::TableSetupColumn("Publish Date");
+		ImGui::TableSetupColumn("Number of Copies Available");
+		ImGui::TableSetupColumn("Number of Pages");
+		ImGui::TableSetupColumn("Language");
+		ImGui::TableSetupColumn("Editor");
+		ImGui::TableSetupColumn("Authors");
+		ImGui::TableSetupColumn("Genres");
+		ImGui::TableSetupColumn("Categories");
+		ImGui::TableHeadersRow();
+		ImGui::EndTable();
+	}
+}
+
+
+void DrawHistoryTable(ImGuiTableFlags flags) {
+	const int nb_col = 9;
+	
+	if (ImGui::Checkbox("Publish date", &checkedP)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("title", &checkedT)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Author", &checkedA)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Grenre", &checkedG)) {
+	}
+	ImGui::SameLine();
+	if (ImGui::Checkbox("Category", &checkedC)) {
+	}
+	char* wanted = (char*)malloc(sizeof(char));
+	ImGui::InputText("##",wanted,100);
+	ImGui::SameLine();
+	if (ImGui::Button("apply")) {
+
+	}
+	if (ImGui::BeginTable("tableHistory", nb_col, flags)) {
+		ImGui::TableSetupColumn("Title");
+		ImGui::TableSetupColumn("Publish Date");
+		ImGui::TableSetupColumn("Number of Copies Available");
+		ImGui::TableSetupColumn("Number of Pages");
+		ImGui::TableSetupColumn("Language");
+		ImGui::TableSetupColumn("Editor");
+		ImGui::TableSetupColumn("Genres");
+		ImGui::TableSetupColumn("Authors");
+		ImGui::TableSetupColumn("Categories");
+		ImGui::TableHeadersRow();
+		ImGui::EndTable();
+	}
+}
+
+
+void MemberInterface() {
+	ImGuiTableFlags flags = ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg;
+	static Pages  current_page = Pages::MainTable;
+	
+	if (ImGui::BeginMainMenuBar()) {
+		if (ImGui::BeginMenu("Books")) {
+			if (ImGui::MenuItem("Home")) {
+				current_page = Pages::MainTable;
+			}
+			if (ImGui::MenuItem("My current borrows")) {
+				current_page = Pages::CurrentBorrows;
+				}
+		
+			if (ImGui::MenuItem("History of borrows")) {
+				current_page = Pages::BorrowHistory;
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+
+	ImVec2 size = ImVec2(1300, 1200);
+
+	switch (current_page) {
+	case Pages::MainTable:
+		ImGui::Begin("Home");
+		ImGui::SetWindowSize(size);
+		DrawMainTable(flags);
+		ImGui::End();
+		ImGui::Render();
+		break;
+	case Pages::CurrentBorrows:
+		ImGui::Begin("Borrows");
+		ImGui::SetWindowSize(size);
+		DrawBorrowsTable(flags);
+		ImGui::End();
+		ImGui::Render();
+		break;
+	case Pages::BorrowHistory:
+		ImGui::Begin("History");
+		ImGui::SetWindowSize(size);
+		DrawHistoryTable(flags);
+		ImGui::End();
+		ImGui::Render();
+		break;
+	}
+}
 
 int main() {
 	glfwInit();
@@ -104,9 +282,7 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// ImGui code here
-		ImGui::Begin("My Imgui window");
-		ImGui::Text("Hello world");
-		ImGui::End();
+		MemberInterface();
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
