d4cdabe7bc39e0c50e52b49de9e076de4596bc72
Marouane <marouanelemghari@gmail.com>
2025-01-05 01:20:17 +0100

    Added pages and fixed some bugs

--- BorrowingsHistoryPage.hpp
+++ BorrowingsHistoryPage.hpp
@@ -2,11 +2,53 @@
 #include "Page.hpp"
 
 class BorrowingsHistoryPage : public Page {
+    BorrowingDAO& borrowingDB;
+    Auth& auth;
+    User currMember;
+    std::vector<Borrowing> borrowings;
 
+    void setBorrowings() {
+        borrowings = borrowingDB.getMemberBorrowings(currMember.getId());
+    }
 public:
-    BorrowingsHistoryPage() {}
+    BorrowingsHistoryPage(BorrowingDAO& borrowingDB, Auth& auth) : borrowingDB(borrowingDB), auth(auth) {
+        currMember = auth.getCurrUser();
+        strncpy_s(title, "Borrowings History", sizeof(title));
+        setBorrowings();
+    }
 
     void render(AppState& state, std::shared_ptr<PageState> pageState) override {
-        ImGui::Text("Example Page");
+        const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+        const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
+        ImVec2 outer_size = ImVec2(0.0f, TEXT_BASE_HEIGHT * 8);
+        if (ImGui::BeginTable("Borrowings History List", 6, flags, outer_size))
+        {
+            ImGui::TableSetupScrollFreeze(0, 1); // Make top row always visible
+            ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Date Borrowed", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Intended Return Date", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Actual Return Date", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Status", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Book Title", ImGuiTableColumnFlags_None);
+            ImGui::TableHeadersRow();
+
+            for (auto& borrowing : borrowings)
+            {
+                ImGui::TableNextRow();
+                ImGui::TableNextColumn();
+                ImGui::Text("%d", borrowing.getId());
+                ImGui::TableNextColumn();
+                ImGui::Text(borrowing.getDateBorrowed().c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text(borrowing.getDateIntendedReturn().c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text(borrowing.getDateActualReturn().c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text(borrowing.getStatus().c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text(borrowing.getBook().getTitle().c_str());
+            }
+            ImGui::EndTable();
+        }
     }
 };
--- MembersPage.hpp
+++ MembersPage.hpp
@@ -1,12 +1,57 @@
 #pragma once
 #include "Page.hpp"
 
-class MembersPage : public Page {
+struct MembersPageState : public PageState {
 
+};
+
+class MembersPage : public Page {
+    vector<User> members;
+    UserDAO& userDB;
+
+    void setMembers() {
+        vector<User> users = userDB.findAllUsers();
+        for (auto& user : users) {
+            if (user.getRole() == UserRole::MEMBER) {
+                members.push_back(user);
+            }
+        }
+    }
 public:
-    MembersPage() {}
+    MembersPage(UserDAO& userDB) : userDB(userDB) {
+        strncpy_s(title, "Members", sizeof(title));
+        setMembers();
+    }
 
     void render(AppState& state, std::shared_ptr<PageState> pageState) override {
-        ImGui::Text("Example Page");
+
+        // When using ScrollX or ScrollY we need to specify a size for our table container!
+        // Otherwise by default the table will fit all available space, like a BeginChild() call.
+        const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+        const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
+        ImVec2 outer_size = ImVec2(0.0f, TEXT_BASE_HEIGHT * 8);
+        if (ImGui::BeginTable("Members List", 4, flags, outer_size))
+        {
+            ImGui::TableSetupScrollFreeze(0, 1); // Make top row always visible
+            ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Email", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("First Name", ImGuiTableColumnFlags_None);
+            ImGui::TableSetupColumn("Last Name", ImGuiTableColumnFlags_None);
+            ImGui::TableHeadersRow();
+
+            for (auto& member : members)
+            {
+                ImGui::TableNextRow();
+                ImGui::TableNextColumn();
+                ImGui::Text("%d", member.getId());
+                ImGui::TableNextColumn();
+                ImGui::Text(member.getEmail().c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text(member.getFirstName().c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text(member.getLastName().c_str());
+            }
+            ImGui::EndTable();
+        }
     }
 };
